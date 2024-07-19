b94b002dc7c4c685fb86956cab1fc3e3d9233473
Yaroslav Shmarov <yshmarov@gmail.com>
2024-07-19 23:41:42 +0200

    authentication works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access
+  allow_unauthenticated_access(only: %i[new create])
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, notice: "Signed in."
     else
       redirect_to new_session_url, alert: "Try another email address or password."
     end
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to new_session_url
+    redirect_to new_session_url, notice: "Signed out."
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,12 +2,22 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get root_url
     assert_response :success
   end
 
   test "should get dashboard" do
-    get home_dashboard_url
+    get dashboard_url
+    assert_redirected_to new_session_url
+
+    User.create!(email_address: "foo@bar.com", password: "password")
+    post session_url, params: { email_address: "foo@bar.com", password: "password" }
+    assert_redirected_to dashboard_url
+    follow_redirect!
     assert_response :success
+
+    delete session_url
+    get dashboard_url
+    assert_redirected_to new_session_url
   end
 end
